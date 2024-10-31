4043c789a2e9fb8a80f0e4d897acd32e902e9f66
Fábio Mezzomo <fmezzomo@net11.com.br>
2024-10-31 18:39:51 -0300

    Changed the code to receive the url to get posts

--- classes/class-rtcamp.php
+++ classes/class-rtcamp.php
@@ -1,5 +1,7 @@
 <?php
 class RTCamp {
+    private $URLAPI = 'https://wptavern.com';
+
     public function __construct() {
         add_action( 'init', array( $this, 'register_slideshow_block' ) );
         add_action( 'enqueue_block_assets', array( $this, 'slideshow_block_assets' ) );
@@ -12,10 +14,16 @@ class RTCamp {
             array( 'wp-blocks', 'wp-element', 'wp-editor' ),
         );
 
-        register_block_type('rtcamp/slideshow', array(
+        register_block_type( 'rtcamp/slideshow', array(
             'editor_script'   => 'rtcamp-slideshow-block',
             'render_callback' => array( $this, 'render_slideshow' ),
-        ) );
+            'attributes' => array(
+                'url' => array(
+                    'type' => 'string',
+                    'default' => $this->URLAPI,
+                ),
+            ),
+        ));
     }
 
     // Enqueue block editor script and style
@@ -34,9 +42,15 @@ class RTCamp {
         );
     }
 
-    public function render_slideshow() {
-        $url      = 'https://wptavern.com/wp-json/wp/v2/posts';
-        $response = wp_remote_get( $url );
+    public function render_slideshow( $attributes ) {
+        $url = !empty($attributes['url']) ? esc_url($attributes['url']) : $this->URLAPI;
+
+        $parsed_url = parse_url($url);
+        $domain     = isset($parsed_url['scheme']) ? $parsed_url['scheme'] . '://' . $parsed_url['host'] : $parsed_url['host'];
+
+        $api_url = rtrim($domain, '/') . '/wp-json/wp/v2/posts';
+
+        $response = wp_remote_get( $api_url );
 
         if ( is_wp_error( $response ) ) {
             return '<div>Error fetching posts.</div>';
@@ -48,22 +62,26 @@ class RTCamp {
             return '<div>No posts found.</div>';
         }
 
-        $output  = '<div class="slideshow">';
+        $output  = '<div class="slideshow-container">';
+        $output .= '    <input type="text" id="url-input" placeholder="Enter URL..." />';
+        $output .= '    <button id="change-slideshow">Apply URL</button>';
+
+        $output .= '    <div class="slideshow">';
+
         foreach ( $posts as $post ) {
-            $output .= '<div class="slide">';
-            $output .= '    <a href="' . esc_url( $post[ 'link' ] ) . '" target="_blank">';
-            $output .= '        <h2>'. esc_html( $post[ 'title' ][ 'rendered' ]) . '</h2>';
-            $output .= '        <img src="' . esc_url( $post[ 'jetpack_featured_media_url' ] ) . '" alt="'. esc_attr( $post[ 'title' ][ 'rendered' ] ) . '" />';
-            $output .= '        <p>' . esc_html( date( 'F j, Y', strtotime( $post[ 'date' ] ) ) ) . '</p>';
-            $output .= '    </a>';
-            $output .= '</div>';
+            $output .= '    <div class="slide">';
+            $output .= '        <a href="' . esc_url( $post[ 'link' ] ) . '" target="_blank">';
+            $output .= '            <h2>'. esc_html( $post[ 'title' ][ 'rendered' ]) . '</h2>';
+            $output .= '            <img src="' . esc_url( $post[ 'jetpack_featured_media_url' ] ) . '" alt="'. esc_attr( $post[ 'title' ][ 'rendered' ] ) . '" />';
+            $output .= '            <p>' . esc_html( date( 'F j, Y', strtotime( $post[ 'date' ] ) ) ) . '</p>';
+            $output .= '        </a>';
+            $output .= '    </div>';
         }
-        $output .= '    <button class="prev">Previous</button>';
-        $output .= '    <button class="next">Next</button>';
+        $output .= '        <button class="prev">Previous</button>';
+        $output .= '        <button class="next">Next</button>';
+        $output .= '    </div>';
         $output .= '</div>';
 
         return $output;
-
-
     }
 }
