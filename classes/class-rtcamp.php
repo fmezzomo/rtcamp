89eeba6768d1bb75ff2d233b7655b10814ebef00
Fábio Mezzomo <fmezzomo@net11.com.br>
2024-10-31 17:11:00 -0300

    Load and print posts data

--- classes/class-rtcamp.php
+++ classes/class-rtcamp.php
@@ -18,6 +18,37 @@ class RTCamp {
     }
 
     public function render_slideshow() {
-        return '<div>No posts found.</div>';
+        $url = 'https://wptavern.com/wp-json/wp/v2/posts';
+        $response = wp_remote_get($url);
+
+        if (is_wp_error($response)) {
+            return '<div>Error fetching posts.</div>';
+        }
+
+        $posts = json_decode(wp_remote_retrieve_body($response), true);
+        
+        if (empty($posts)) {
+            return '<div>No posts found.</div>';
+        }
+
+        // Build slideshow HTML
+        ob_start(); 
+        ?>
+        <div class="slideshow">
+            <?php foreach ($posts as $post): ?>
+                <div class="slide" >
+                    <a href="<?php echo esc_url($post['link']); ?>">
+                        <h2><?php echo esc_html($post['title']['rendered']); ?></h2>
+                        <img src="<?php echo esc_url($post['jetpack_featured_media_url']); ?>" alt="<?php echo esc_attr($post['title']['rendered']); ?>" />
+                        <p><?php echo esc_html(date('F j, Y', strtotime($post['date']))); ?></p>
+                    </a>
+                </div>
+            <?php endforeach; ?>
+            <button class="prev">Previous</button>
+            <button class="next">Next</button>
+        </div>
+        
+        <?php
+        return ob_get_clean();
     }
 }
