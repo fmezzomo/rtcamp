d6172198bbd4b99dcbdb83e5d7a79befd79c7ded
Fábio Mezzomo <fmezzomo@net11.com.br>
2024-10-31 17:29:59 -0300

    Changed the way to show data

--- classes/class-rtcamp.php
+++ classes/class-rtcamp.php
@@ -8,13 +8,13 @@ class RTCamp {
     public function register_slideshow_block() {
         wp_register_script(
             'rtcamp-slideshow-block',
-            plugins_url('../js/block.js', __FILE__),
-            array('wp-blocks', 'wp-element', 'wp-editor'),
+            plugins_url( '../js/block.js', __FILE__ ),
+            array( 'wp-blocks', 'wp-element', 'wp-editor' ),
         );
 
         register_block_type('rtcamp/slideshow', array(
-            'editor_script' => 'rtcamp-slideshow-block',
-            'render_callback' => array($this, 'render_slideshow'),
+            'editor_script'   => 'rtcamp-slideshow-block',
+            'render_callback' => array( $this, 'render_slideshow' ),
         ) );
     }
 
@@ -22,12 +22,12 @@ class RTCamp {
     function slideshow_block_assets() {
         wp_enqueue_style(
             'my-slideshow-style',
-            plugins_url('../css/style.css', __FILE__),
+            plugins_url( '../css/style.css', __FILE__ ),
         );
 
         wp_enqueue_script(
             'my-slideshow-script',
-            plugins_url('../js/slideshow.js', __FILE__),
+            plugins_url( '../js/slideshow.js', __FILE__ ),
             array(),
             '1.0',
             true
@@ -35,36 +35,35 @@ class RTCamp {
     }
 
     public function render_slideshow() {
-        $url = 'https://wptavern.com/wp-json/wp/v2/posts';
-        $response = wp_remote_get($url);
+        $url      = 'https://wptavern.com/wp-json/wp/v2/posts';
+        $response = wp_remote_get( $url );
 
-        if (is_wp_error($response)) {
+        if ( is_wp_error( $response ) ) {
             return '<div>Error fetching posts.</div>';
         }
 
-        $posts = json_decode(wp_remote_retrieve_body($response), true);
+        $posts = json_decode( wp_remote_retrieve_body( $response ), true );
         
-        if (empty($posts)) {
+        if ( empty( $posts ) ) {
             return '<div>No posts found.</div>';
         }
 
-        // Build slideshow HTML
-        ob_start();
-        ?>
-        <div class="slideshow">
-            <?php foreach ($posts as $post): ?>
-                <div class="slide" style="display: none;">
-                    <a href="<?php echo esc_url($post['link']); ?>">
-                        <h2><?php echo esc_html($post['title']['rendered']); ?></h2>
-                        <img src="<?php echo esc_url($post['jetpack_featured_media_url']); ?>" alt="<?php echo esc_attr($post['title']['rendered']); ?>" />
-                        <p><?php echo esc_html(date('F j, Y', strtotime($post['date']))); ?></p>
-                    </a>
-                </div>
-            <?php endforeach; ?>
-            <button class="prev">Previous</button>
-            <button class="next">Next</button>
-        </div>
-        <?php
-        return ob_get_clean();
+        $output  = '<div class="slideshow">';
+        foreach ( $posts as $post ) {
+            $output .= '<div class="slide" style="display: none;">';
+            $output .= '    <a href="' . esc_url( $post[ 'link' ] ) . '">';
+            $output .= '        <h2>'. esc_html( $post[ 'title' ][ 'rendered' ]) . '</h2>';
+            $output .= '        <img src="' . esc_url( $post[ 'jetpack_featured_media_url' ] ) . '" alt="'. esc_attr( $post[ 'title' ][ 'rendered' ] ) . '" />';
+            $output .= '        <p>' . esc_html( date( 'F j, Y', strtotime( $post[ 'date' ] ) ) ) . '</p>';
+            $output .= '    </a>';
+            $output .= '</div>';
+        }
+        $output .= '    <button class="prev">Previous</button>';
+        $output .= '    <button class="next">Next</button>';
+        $output .= '</div>';
+
+        return $output;
+
+
     }
 }
