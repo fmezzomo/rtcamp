89c3c68a846f1ca0b382cb2bcd4639bf31d93003
Fábio Mezzomo <fmezzomo@net11.com.br>
2024-10-31 16:59:12 -0300

    Changed script

--- js/block.js
+++ js/block.js
@@ -1,8 +1,28 @@
-const { registerBlockType } = wp.blocks;
+( function( blocks, element ) {
+    var el = element.createElement;
 
-registerBlockType('rtcamp/slideshow', {
-    title: 'rtCamp Block Slideshow',
-    description: 'Adds a slideshow block to display recent posts using the WP REST API in the Gutenberg editor.',
-    icon: 'format-image',
-    category: 'widgets'
-});
+    var blockStyle = {
+        backGroundColor: '#900',
+        color: '#000'
+    }
+
+    blocks.registerBlockType('rtcamp/slideshow', {
+        title: 'rtCamp Block Slideshow',
+        description: 'Adds a slideshow block to display recent posts using the WP REST API in the Gutenberg editor.',
+        icon: 'format-image',
+        category: 'widgets',
+        edit: function() {
+            return el(
+                'p',
+                { style: blockStyle },
+                'Slideshow Block',
+            );
+        },
+        save: function() {
+            return null;
+        }
+    });
+}(
+    window.wp.blocks,
+    window.wp.element
+) );
